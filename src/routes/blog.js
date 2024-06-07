c0ac42dc9dfd4e4f9b3a81b1cfea2837a3d04bb2
LeTranThai <20130400@st.hcmuaf.edu.vn>
2024-06-07 15:20:10 +0700

    Update code: Blog

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { default as blogController } from '../app/controllers/BlogController.js';
+import multer from 'multer';
+import blogController from '../app/controllers/BlogController.js';
 const blogRouter = express.Router();
+const upload = multer();
 
-blogRouter.get('/', blogController.get_all);
-blogRouter.get('/:slug', blogController.find);
+blogRouter.get('/all', blogController.get_all);
+blogRouter.get('/all-size', blogController.get_all_size);
+blogRouter.post('/:id/edit', upload.single('image'), blogController.update);
+blogRouter.post('/', upload.single('img'), blogController.create);
+
+blogRouter.get('/', blogController.get_all_limit);
+blogRouter.get('/:id', blogController.find);
 
 export { blogRouter };
